8824626b389b07256eb08228c732b84db4de56cf
Djordje Ljubinkovic <ljubinkovicdj93@gmail.com>
2019-04-04 17:19:30 +0200

    Creates a Routable protocol which handles basic CRUD operations. Helps with specifying which endpoint can perform which CRUD operation.

--- Sources/App/Controllers/LanguagesController.swift
+++ Sources/App/Controllers/LanguagesController.swift
@@ -10,11 +10,46 @@ struct LanguagesController: RouteCollection {
     func boot(router: Router) throws {
         let languagesRoutes = router.grouped("api", "languages")
         
+        // Creatable
+        languagesRoutes.post(Language.self, use: createHandler)
+        
         languagesRoutes.get(use: getAllLanguagesHandler)
+        
+        // Searchable
+        languagesRoutes.get("search", use: searchHandler)
     }
     
     func getAllLanguagesHandler(_ req: Request) throws -> Future<[Language]> {
         return Language.query(on: req).all()
+    }
+}
+
+extension LanguagesController: CRUDRepresentable, Queryable {
+    typealias T = Language
+    
+    func updateHandler(_ req: Request) throws -> Future<Language> {
+        return try flatMap(
+            to: Language.self,
+            req.parameters.next(Language.self),
+            req.content.decode(Language.self)
+        ) { language, updatedLanguage in
+            language.name = updatedLanguage.name
+            return language.save(on: req)
+        }
+    }
+    
+    func searchHandler(_ req: Request) throws -> Future<[Language]> {
+        guard let searchTerm = req
+            .query[String.self, at: "term"] else {
+                throw Abort(.badRequest)
+        }
+        return Language.query(on: req)
+            .filter(\.name == searchTerm)
+            .all()
+    }
+    
+    func sortedHandler(_ req: Request) throws -> Future<[Language]> {
+        return Language.query(on: req).sort(\.name, .ascending).all()
     }
 }
 
---
+++ Sources/App/Routable/Routable.swift
@@ -0,0 +1,72 @@
+// Project: LingoChatBE
+//
+// Created on Thursday, April 04, 2019.
+// 
+
+import Vapor
+import Fluent
+
+typealias CRUDRepresentable = Creatable & Retrievable & Updatable & Deletable
+typealias Queryable = Searchable & Sortable
+
+protocol Routable {
+    associatedtype T where T: Content & Model & Parameter, T.ResolvedParameter == Future<T>
+}
+
+protocol Creatable: Routable {
+    func createHandler(_ req: Request, type: T) throws -> Future<T>
+}
+
+extension Creatable {
+    func createHandler(_ req: Request, type: T) throws -> Future<T> {
+        return type.save(on: req)
+    }
+}
+
+protocol Retrievable: Routable {
+    func getAllHandler(_ req: Request) throws -> Future<[T]>
+    func getHandler(_ req: Request) throws -> Future<T>
+    func getFirstHandler(_ req: Request) throws -> Future<T>
+}
+
+extension Retrievable {
+    func getAllHandler(_ req: Request) throws -> Future<[T]> {
+        return T.query(on: req).all()
+    }
+    
+    func getHandler(_ req: Request) throws -> Future<T> {
+        return try req.parameters.next(T.self)
+    }
+    
+    func getFirstHandler(_ req: Request) throws -> Future<T> {
+        return T.query(on: req)
+            .first()
+            .unwrap(or: Abort(.notFound))
+    }
+}
+
+protocol Updatable: Routable {
+    func updateHandler(_ req: Request) throws -> Future<T>
+}
+
+protocol Deletable: Routable {
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus>
+}
+
+extension Deletable {
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req
+            .parameters
+            .next(T.self)
+            .delete(on: req)
+            .transform(to: .noContent)
+    }
+}
+
+protocol Searchable: Routable {
+    func searchHandler(_ req: Request) throws -> Future<[T]>
+}
+
+protocol Sortable: Routable {
+    func sortedHandler(_ req: Request) throws -> Future<[T]>
+}
