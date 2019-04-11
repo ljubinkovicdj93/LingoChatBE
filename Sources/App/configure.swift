eb68c04ea3e877d13ad7c15b4556232933a5ec96
Djordje Ljubinkovic <ljubinkovicdj93@gmail.com>
2019-04-11 10:24:13 +0200

    Revert "Uses database seeding to add the AdminUser when the app starts because the User create endpoint is protected by authentication."
    This reverts commit 22ebdf0e4e276a62901f6b1dc11084714f93c5ab.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -72,7 +72,5 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     migrations.add(model: Token.self, database: .psql)
     
-    migrations.add(migration: AdminUser.self, database: .psql)
-    
     services.register(migrations)
 }
